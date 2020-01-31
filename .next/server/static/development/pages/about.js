d6140891cdac0bb0f375002066493d3ed1ef5909
Tuan <tuan@Tuans-MBP.net.fpt>
2020-01-31 10:01:32 +0700

    jfaf

--- .next/server/static/development/pages/about.js
+++ .next/server/static/development/pages/about.js
@@ -1866,55 +1866,49 @@ module.exports = __webpack_require__(/*! ./dist/client/link */ "./node_modules/n
 
 /***/ }),
 
-/***/ "./pages/index.js":
+/***/ "./pages/about.js":
 /*!************************!*\
-  !*** ./pages/index.js ***!
+  !*** ./pages/about.js ***!
   \************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return About; });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _components_MyLayout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/MyLayout */ "./components/MyLayout.js");
-var _jsxFileName = "/Users/tuan/Documents/Tuan/Nextjs/hello-nextjs/pages/index.js";
+var _jsxFileName = "/Users/tuan/Documents/Tuan/Nextjs/hello-nextjs/pages/about.js";
 
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
-function Index() {
-  return __jsx("div", {
+function About() {
+  return __jsx(_components_MyLayout__WEBPACK_IMPORTED_MODULE_1__["default"], {
     __source: {
       fileName: _jsxFileName,
       lineNumber: 5
     },
     __self: this
-  }, __jsx(_components_MyLayout__WEBPACK_IMPORTED_MODULE_1__["default"], {
+  }, __jsx("p", {
     __source: {
       fileName: _jsxFileName,
       lineNumber: 6
     },
     __self: this
-  }, __jsx("p", {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 7
-    },
-    __self: this
-  }, "Hello Next.js")));
+  }, "This is the about page xcxcxcxcfjsfhjei"));
 }
 
 /***/ }),
 
 /***/ 4:
 /*!******************************!*\
-  !*** multi ./pages/index.js ***!
+  !*** multi ./pages/about.js ***!
   \******************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/tuan/Documents/Tuan/Nextjs/hello-nextjs/pages/index.js */"./pages/index.js");
+module.exports = __webpack_require__(/*! /Users/tuan/Documents/Tuan/Nextjs/hello-nextjs/pages/about.js */"./pages/about.js");
 
 
 /***/ }),
@@ -2030,4 +2024,4 @@ module.exports = require("url");
 /***/ })
 
 /******/ });
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=about.js.map
--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from "../components/MyLayout";
 export default function About() {
   return (
     <Layout>
-      <p>This is the about page</p>
+      <p>This is the about page xcxcxcxcfjsfhjei</p>
     </Layout>
   );
 }
